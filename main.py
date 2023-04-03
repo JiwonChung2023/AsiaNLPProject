438dc2f9f2b2f4d0e9aeb516fe259f75dede3f8e
Jiwon Chung <0000againism@korea.ac.kr>
2023-04-03 12:50:06 +0900

    new file:   db/navermv.db 	new file:   getData.py 	modified:   main.py

--- main.py
+++ main.py
@@ -1,131 +1,124 @@
-# 라이브러리 가져오기
-
-import sqlite3
+#%%
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import requests
-from bs4 import BeautifulSoup as bsp
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from bs4 import BeautifulSoup as bsp
-import time
+import pycaret
+import sqlite3
+from kiwipiepy import Kiwi
+kiwi=Kiwi()
+kiwi.prepare()
+# %%
+def getPos(txt='안녕하세요 여러분 여러분 반갑습니다.'):
+    res=kiwi.analyze(txt)
+    badwords=['']
+    #    pos=['NNG','NNP','NNB','NR','NP','VV','VA','VX','VCP','VCN','MM','MA','MAJ']
+    pos=['NNG']
+    tokens=[]
+    for tkn in res[0][0]:
+        for bad in badwords:
+            src=list(tkn)
+            if(src[0]!=bad):
+                #print(src)
+                for p in pos:
+                    #print(src[1],p)
+                    if(src[1].find(p)>-1):
+                        tokens.append(src[0])
+                        break
+    return tokens
+# %%
+# DB에서 자료 가져오기
+sql="select comment from NaverNewsComments"
+db='./Database Folder/myResultDB.db'
+with sqlite3.connect(db) as conn:
+    cur=conn.cursor()
+    res=cur.execute(sql).fetchall()
+txt=[]
+for r in res:
+    txt.append(r[-1])
+print(txt)
+# 비교할 대상을 설정
+txt[0]='조국 조민'
+txt
+# %%
+# 영어식 한글 표현    
+tks=getPos(txt)
+print(tks)
+# %%
+# CBOW : 카운트기반 BOW
+from sklearn.feature_extraction.text import CountVectorizer
+# TF_IDF (Term Frequency - Inverse Document Frequency)
+# 숫자기반 문서 벡터 - 단어의 중요성 기반의 벡터
+from sklearn.feature_extraction.text import TfidfVectorizer
 
-# 웹드라이버 가져오기
-cdriver='./driver/chromedriver.exe'
-driver=webdriver.Chrome(cdriver)
-driver.set_window_position(0,50)
-driver.set_window_size(800, 1300)
-url='https://media.naver.com/press/001/ranking'
-driver.get(url)
-time.sleep(2) # 딜레이를 통해 웹이 로딩되는 시간을 기다려줌
-# 디비 정의 및 함수 정의하기
 
-dfile='./db/navernews.db'
-# "select" 구문
-def sqlPrs(sql='',d=[],opt=1):
-    with sqlite3.connect(dfile) as conn:
-        cur=conn.cursor()
-        # select 문의 경우 실행 후 데이터 반환이 필요하다!
-        if opt==1:
-            res=cur.execute(sql).fetchall()
-        elif opt==2:
-            # insert: insert into mvreview (rid,title,href,point,user,rday,content) values(?,?,?,?,?,?,?)
-            res=cur.execute(sql,d)
-        else:
-            cur.execute(sql)
-            res=0
-        conn.commit()
-    return(res)
+def getCBOW(texts=[],opt='CBOW'):
+    copus=[]
+    # 영어화 문서로 변형
+    tarrs=[]
+    for t in texts:
+        tarr=getPos(t)
+        tarrs.append(tarr)
+        copus.append(' '.join(tarr))
+    if opt=='CBOW':    
+        vec=CountVectorizer()   
+    else:
+        vec=TfidfVectorizer()
+    vtr=vec.fit_transform(copus)
+    cols=[t for t,n,in sorted(vec.vocabulary_.items())]
+    return (cols,vtr.toarray(),tarrs)
 
-def upgradeScrapeComments():
-    pInfo=driver.find_element(by=By.CSS_SELECTOR,value='div.u_cbox_userinfo_meta > div.u_cbox_userinfo_meta_header > button')
-    pInfoCon=bsp(pInfo.get_attribute('data-param'),'html.parser')
-    li1=str(pInfoCon).split(',')
-    nick=li1[6].replace(" 'nickName':'",'').split("'")[0] # 닉네임 추출
-    nid=li1[7].replace(" 'maskedId':'",'').split("'")[0] # 마스크드 아이디 추출
-    comms=driver.find_elements(by=By.CSS_SELECTOR,value='.u_cbox_area')
-    jw=0
-    for com in comms:
-        if (com.text==''):
-            pass
-        else:
-            if (jw==0):
-                jw+=1
-                pass
-            else:
-                li2=com.text.split('\n')
-                comment=li2[0]
-                date=li2[1]
-                whereat=li2[3]
-                if (date=='댓글모음'):
-                    pass
-                else:
-                    d=[whereat,Ntitle,nid,nick,date,comment] # 데이터베이스 입력
-                    sql='insert into projectTable (whereat,Ntitle,nid,nick,date,comment) values(?,?,?,?,?,?)'
-                    sqlPrs(sql,d,2)
-        
-# 오류 나면 이거 다시 실행시켜라
-def clickIt(ssel,drv=driver):
-    drv.find_element(by=By.CSS_SELECTOR,value=ssel).click()
-    
-def goScroll(level=0):
-    #dheight=driver.execute_script('return document.documentElement.scrollHeight')
-    dheight=driver.execute_script('window.scrollTo(0,{})'.format(level))
+# %%
+cols,data,tarrs=getCBOW(txt,'TFIDF')
+tedf=pd.DataFrame(data,columns=cols)
+print(tedf)
+# %%
+# 코사인 유사도
+def cossim(x,y):
+    x=np.array(x)
+    y=np.array(y)
+    csim=np.dot(x,y)/(np.linalg.norm(x)*np.linalg.norm(y))
+    return csim
+#%%
+text1=tedf.iloc[:1,:]
+print(text1)
+corrList=[]
+for i,v in tedf.iloc[1:,:].iterrows():
+    #print(txt[0])
+    #print(txt[i],':',cossim(text1,v))
+    if (cossim(text1,v)>=0.1):
+        corrList.append(txt[i])
+# %%
 
-# 본 파일
-for k in [10,12,13]: # 각 언론사 홈페이지로 이동하기
-    try:
-        if (k<10):
-            uurl='https://media.naver.com/press/00'+str(k)+'/ranking'
-        else:
-            uurl='https://media.naver.com/press/0'+str(k)+'/ranking'
-        driver.get(uurl)
-        time.sleep(2)
+#%%
+text1=tedf.iloc[:1,:]
+print(text1)
+# %%
+for i,v in tedf.iloc[1:,:].iterrows():
+    #print(v)
+    print(txt[i],':',np.sum(np.square(text1.values-v.values)))
+# %%
+tedf
+# %%
+import gensim
+#%%
+cols,data,tarrs=getCBOW(corrList,'TFIDF')
+#%%
+print(tarrs)
+#%%
+from gensim import corpora
+dic=corpora.Dictionary(tarrs) # 사전만들기
+print(len(dic))
 
-        clickIt('#ct > div.press_ranking_home > ul > li:nth-child(2) > a') # 댓글 많은 순 정렬
-        time.sleep(2)
-
-        print('\n현재 {}번째 언론사에서 스크레이핑 중'.format(k),end='')
-
-        for i in range(1,21):
-            try:
-                print('.',end='') # 몇 번째 기사인지 알려줌
-
-                driver.find_element(by=By.XPATH,value='//*[@id="ct"]/div[2]/div[2]/ul/li[{}]/a'.format(i)).click() #뉴스 스무 개 차례대로 클릭하기
-                time.sleep(2)
-                Ntitle=driver.find_element(by=By.CSS_SELECTOR,value='#title_area > span').text
-                clickIt('#cbox_module > div.u_cbox_wrap.u_cbox_ko.u_cbox_type_sort_favorite > div.u_cbox_view_comment > a') # 댓글 더보기 
-                time.sleep(2)
-                try:
-                    clickIt('#cbox_module > div.u_cbox_wrap.u_cbox_ko.u_cbox_type_sort_favorite > div.u_cbox_paginate > a') # 댓글 더보기 2
-                    time.sleep(2)
-
-                    clickIt('#cbox_module > div.u_cbox_wrap.u_cbox_ko.u_cbox_type_sort_favorite > div.u_cbox_paginate > a') # 댓글 더보기 3
-                    time.sleep(2)
-
-                    clickIt('#cbox_module > div.u_cbox_wrap.u_cbox_ko.u_cbox_type_sort_favorite > div.u_cbox_paginate > a') # 댓글 더보기 4
-                    time.sleep(2)
-                except:
-                    pass
-            except:
-                pass
-
-            goScroll() # 맨 위로 스크롤하기
-            comButtons=driver.find_elements(by=By.CSS_SELECTOR,value='div.u_cbox_info > span.u_cbox_info_main > button') # 이용자 최근 댓글 창으로 들어가기
-            for comB in comButtons:
-                comB.click()
-                time.sleep(3)
-                try:
-                    upgradeScrapeComments() # 댓글 가져오기
-                except:
-                    pass
-                driver.back()
-
-            driver.back()
-            time.sleep(2)
-    except:
-        pass
-
-print('*******The program has successfully ended*******')
+copus=[dic.doc2bow(tarr) for tarr in tarrs]
+copus
+lda=gensim.models.ldamodel.LdaModel(copus,num_topics=3)
+lda.print_topics(num_words=5)
+# %%
+#pip install pyLDAvis
+import pyLDAvis
+import pyLDAvis.gensim_models as gm
+gvis=gm.prepare(lda,copus,dic)
+gvis
+# %%
+pyLDAvis.display(gvis)
+# %%
